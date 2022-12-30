ccc9975fe11432cd755682d95fc976f4f6e7389e
Dilmurod Yangiboev <dilmurod.yangiboev@gmail.com>
2022-12-30 23:01:23 +0100

    intro: image process and uploader

--- server.go
+++ server.go
@@ -6,80 +6,14 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-
-	fmt.Fprintf(w, "hello\n")
-}
-
-func image(w http.ResponseWriter, req *http.Request) {
-	images := dell()
-	// Just a simple GET request to the image URL
-	// We get back a *Response, and an error
-	for _, image := range images {
-		fmt.Println(image)
-		res, err := http.Get(image)
-
-		if err != nil {
-			log.Fatalf("http.Get -> %v", err)
-		}
-
-		// We read all the bytes of the image
-		// Types: data []byte
-		data, err := ioutil.ReadAll(res.Body)
-
-		if err != nil {
-			log.Fatalf("ioutil.ReadAll -> %v", err)
-		}
-
-		// You have to manually close the body, check docs
-		// This is required if you want to use things like
-		// Keep-Alive and other HTTP sorcery.
-		res.Body.Close()
-		// You can now save it to disk or whatever...
-		ioutil.WriteFile("web."+format(image), data, 0666)
-	}
-	log.Println("I saved your image buddy!")
-}
-
-func upload() {
-	url := "https://storage.bunnycdn.com/storageZoneName/path/fileName"
-
-	req, _ := http.NewRequest("PUT", url, nil)
-
-	req.Header.Add("content-type", "application/octet-stream")
-
-	res, _ := http.DefaultClient.Do(req)
-
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-
-	fmt.Println(res)
-	fmt.Println(string(body))
-}
-
-// possible requests:
-// /fit-in/128x64/filters:format(webp)/https://raw.githubusercontent.com/cshum/imagor/master/testdata/gopher.png
-// /fit-in/128x64/filters:format(jpeg)/https://raw.githubusercontent.com/cshum/imagor/master/testdata/gopher.png
-// /fit-in/128x64/filters:format(png)/https://raw.githubusercontent.com/cshum/imagor/master/testdata/gopher.png
-
-// https://vediagames.b-cdn.net/1641834775838.jpeg
-// https://vediagames.b-cdn.net/download%20(1).jpeg
-// https://vediagames.b-cdn.net/download.jpeg
-func main() {
-
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/image", image)
-
-	http.ListenAndServe(":8090", nil)
-}
-
-func dell() []string {
+func generateURL() []string {
 	images := []string{
 		"fit-in/128x64/filters:format(webp)/https://raw.githubusercontent.com/cshum/imagor/master/testdata/gopher.png",
 		"fit-in/128x64/filters:format(jpeg)/https://vediagames.b-cdn.net/download%20(1).jpeg",
@@ -91,6 +25,20 @@ func dell() []string {
 	}
 	return res
 }
+
+func processImage() {
+	for _, image := range generateURL() {
+		res, err := http.Get(image)
+
+		if err != nil {
+			log.Fatalf("http.Get -> %v", err)
+		}
+
+		upload(image+format(image), res.Body)
+	}
+	log.Println("I saved your image buddy!")
+}
+
 func sign(path, secret string) string {
 	hash := hmac.New(sha256.New, []byte(secret))
 	hash.Write([]byte(path))
@@ -113,4 +61,31 @@ func format(str string) string {
 		}
 	}
 	return ""
+}
+
+func upload(fileName string, body io.Reader) {
+	url := "https://storage.bunnycdn.com/vediagames/vediagames/" + fileName
+
+	req, err := http.NewRequest("PUT", url, body)
+	if err != nil {
+		log.Println(err)
+	}
+	req.Header.Add("AccessKey", "88800820-6c69-4dba-9a3919b168f5-dbc5-4262")
+	req.Header.Add("content-type", "application/octet-stream")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println(err)
+	}
+	defer res.Body.Close()
+	response, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(res)
+	fmt.Println(string(response))
+}
+
+func main() {
+	processImage()
 }
